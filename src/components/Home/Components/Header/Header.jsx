407f86ac963ae315741f61f49f289c44b14b7126
Minh Tài <tranminhtai365@gmail.com>
2021-06-17 17:40:06 +0700

    edit header navbar

--- src/components/Home/Components/Header/Header.jsx
+++ src/components/Home/Components/Header/Header.jsx
@@ -55,33 +55,50 @@ class Header extends Component {
         return (
             <header id='header' className='container-fluid w-100 px-0 '>
                 <div className='row border-bottom info d-md-flex align-items-md-center d-none px-2'>
-                    <div className="col-md-6 "> <span>{this.props.info.name} xin kính chào quý khách</span></div>
-                    <div className="col-md-6 d-flex justify-content-end">Hotline miễn phí 24/7: {this.props.info.phone}</div>
+                    <div className="col-md-7 "> <span>{this.props.info.name} xin kính chào quý khách</span></div>
+
+                    <div className="col-md-5 d-flex justify-content-end">Hotline miễn phí 24/7: {this.props.info.phone}</div>
                 </div>
                 <Headroom >
-                    <div style={{ fontSize: '20px' }}>
+                    <div>
                         <Navbar color="light" light expand="md" className='w-100 px-0 border-0 '>
-                            <Row lg={2} className="w-100 bs--gutter">
-                                <Col lg={4} className='d-md-flex justify-content-md-center'>
+                            <Row className="w-100 bs--gutter d-md-flex justify-content-md-between">
+                                <Col md={4} className='d-flex justify-content-md-center justify-content-between align-items-center'>
                                     <NavbarBrand >
-                                        <img className="webLogo" src={this.props.info.logo} width="150" height="50" alt="logo" />
+                                        <img className="webLogo" src={this.props.info.logo} width="120" height="50" alt="logo" />
                                     </NavbarBrand>
-                                    <Col lg={12} className='d-flex align-items-center justify-content-end d-lg-none'>
+                                    <div className="col-6 d-md-none">
+                                        <Form className="my-2 w-sm-75 my-lg-0 pr-2 d-flex justify-content-end align-items-center box-search ">
+                                            <input type="text" placeholder="Nhập tên sản phẩm..." />
+                                            <span className="">
+                                                <i className="fa fa-search" aria-hidden="true"></i>
+                                            </span>
+                                        </Form>
+                                    </div>
+                                    <Col lg={6} className='d-flex align-items-center justify-content-end d-lg-none'>
                                         <NavbarToggler onClick={this.toggle} />
                                     </Col>
                                 </Col>
-                                <Col lg={8} md={12} className='d-flex align-items-center justify-content-md-center justify-content-end'>
+                                <Col md={4} className="text-right d-md-block d-none">
+                                    <Form className="my-2 my-lg-0 pr-2 d-flex justify-content-start align-items-center box-search ">
+                                        <input type="text" placeholder="Nhập tên sản phẩm..." />
+                                        <span>
+                                            <i className="fa fa-search" aria-hidden="true"></i>
+                                        </span>
+                                    </Form>
+                                </Col>
+                                <Col md={12} style={{fontSize:'20px',letterSpacing:'2px'}} className='d-flex align-items-center pe-md-5 pe-0 justify-content-end'>
                                     <Row lg={1}  >
                                         <Collapse className={isOpen ? 'd-block' : 'd-none'} navbar>
                                             <Nav navbar className="w-100">
-                                                <Col lg={9} className="d-flex flex-md-row flex-column align-items-md-center align-items-end px-md-0 px-3">
+                                                <Col lg={12} className="d-flex flex-md-row flex-column align-items-md-center align-items-end px-md-0 px-3">
                                                     <NavItem>
-                                                        <NavLink activeClassName='choose' className='nav-link pr-md-0' onClick={this.toggle} to='/home' >Trang chủ</NavLink>
+                                                        <NavLink activeClassName='choose' className='nav-link pe-md-4 ' onClick={this.toggle} to='/home' >Trang chủ</NavLink>
                                                     </NavItem>
                                                     <NavItem>
-                                                        <NavLink activeClassName='choose' className='nav-link pr-md-0' onClick={this.toggle} to='/introduce' >Giới thiệu</NavLink>
+                                                        <NavLink activeClassName='choose' className='nav-link pe-md-4' onClick={this.toggle} to='/introduce' >Giới thiệu</NavLink>
                                                     </NavItem>
-                                                    <UncontrolledDropdown nav inNavbar className='pr-md-0 text-end'>
+                                                    <UncontrolledDropdown nav inNavbar className='pr-md-0 text-end pe-md-4'>
                                                         <DropdownToggle nav caret>
                                                             Sản phẩm
                                                         </DropdownToggle>
@@ -92,13 +109,13 @@ class Header extends Component {
                                                                         <NavLink activeClassName='choose' onClick={() => {
                                                                             this.props.getCateId(ca._id);
                                                                             this.toggle();
-                                                                        }} className='nav-link' to={'/catelogy/' + To_slug(ca.name)} >{ca.name}</NavLink>
+                                                                        }} className='nav-link ' to={'/catelogy/' + To_slug(ca.name)} >{ca.name}</NavLink>
                                                                     </DropdownItem>
                                                                 </div>
                                                             })}
                                                         </DropdownMenu>
                                                     </UncontrolledDropdown>
-                                                    <UncontrolledDropdown nav inNavbar className='pr-md-0 text-end'>
+                                                    <UncontrolledDropdown nav inNavbar className='pr-md-0 text-end pe-md-4'>
                                                         <DropdownToggle nav caret>
                                                             Dịch vụ
                                                         </DropdownToggle>
@@ -118,11 +135,6 @@ class Header extends Component {
                                                     <NavItem>
                                                         <NavLink activeClassName='choose' className='nav-link' to='/contact' >Liên hệ</NavLink>
                                                     </NavItem>
-                                                </Col>
-                                                <Col lg={3}>
-                                                    <Form className="my-2 my-lg-0 pr-2 d-flex justify-content-end align-items-center ">
-                                                        <Input type="text" placeholder="Search" />
-                                                    </Form>
                                                 </Col>
                                             </Nav>
                                         </Collapse>
